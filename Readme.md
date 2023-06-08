f3fadb375d6dcffa0effbef5e9d16aa6e0615736
Zinkelburger <andrewlbernal@gmail.com>
2023-06-07 23:55:19 -0400

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+## 
+The goal of this project is to have it host a website where you can input FENs, and these will be stored for later retreival in a TWIC 
+
+I will 
---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+
+// Components
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Events from "./pages/Events";
+
+// Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <NavBar />
+
+        {/* LINKS  */}
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/events" component={Events} />
+        </Switch>
+
+        <Footer />
+      </Router>
+    </>
+  );
+}
+
+export default App;
